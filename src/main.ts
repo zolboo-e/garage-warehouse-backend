d5b5293bf92d23258b21de764917aeb033fbc3e8
zolboo-e <90889294+zolboo-e@users.noreply.github.com>
2023-06-12 15:29:19 +0800

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "@/modules/app/app.module";
 
 const bootstrap = async () => {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: "http://localhost:3000",
+    },
+  });
   await app.listen(3001);
 };
 bootstrap();
